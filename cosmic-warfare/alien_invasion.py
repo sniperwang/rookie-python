391cfa0fe3bff1d9ab23eecc2e4a49b330d9dc31
wangteng <wangteng@duodainfo.cn>
2020-03-04 23:48:36 +0800

    add ship

--- cosmic-warfare/alien_invasion.py
+++ cosmic-warfare/alien_invasion.py
@@ -1,7 +1,9 @@
 import sys
 
 import pygame
+from pygame.sprite import Group
 
+from alien import Alien
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -14,11 +16,15 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     ship = Ship(ai_settings, screen)
+    bullets = Group()
+    alien = Alien(ai_settings, screen)
 
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        # bullets.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, alien, bullets)
 
 
 run_game()
--- cosmic-warfare/settings.py
+++ cosmic-warfare/settings.py
@@ -2,8 +2,13 @@
 class Settings():
 
     def __init__(self):
-        self.screen_width = 900
-        self.screen_height = 600
+        self.screen_width = 300
+        self.screen_height = 500
         self.bg_color = (230, 230, 230)
-        self.ship_speed_factor = 1.5
-0
+        self.ship_speed_factor = 0.3
+
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
